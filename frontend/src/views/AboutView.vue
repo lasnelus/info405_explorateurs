21984c1fc135092ee54bb65b0331d52d818bd2a2
lasnelus <malabretienne@gmail.com>
2026-01-13 23:20:06 +0100

    feat: tried to learn how vue.js work

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>À propos</h1>
+    <p>Contenu de la page</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Votre logique TypeScript ici
+</script>
+
+<style scoped>
+/* Vos styles ici */
+</style>
---
+++ frontend/src/views/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>À propos</h1>
+    <p>Contenu de la page</p>
+    <button>manger de la pizza</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Votre logique TypeScript ici
+</script>
+
+<style scoped>
+/* Vos styles ici */
+</style>
