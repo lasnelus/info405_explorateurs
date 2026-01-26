32f617f5eb035aebfe9c7d75014372c29dd2f1eb
Tomme <nilsbriffod@gmail.com>
2026-01-26 18:40:35 +0100

    SCRUM-46: add Swagger routes for Child entity (WIP)

--- backend/src/child/child.service.ts
+++ backend/src/child/child.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ChildService {}
+export class ChildService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllChild() {
+    return this.prisma.child.findMany();
+  }
+
+  async getChildById(@Param('id') id: string) {
+    return this.prisma.child.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async addChild(data: { firstName: string; lastName: string }) {
+    const child = await this.prisma.child.create({
+      data: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+      },
+    });
+    return { data: child };
+  }
+
+  async deleteChild(@Param('id') id: string) {
+    try {
+      await this.prisma.child.delete({
+        where: { id: id },
+      });
+      return { message: `Child with id ${id} deleted successfully.` };
+    } catch {
+      throw new Error(`Child with id ${id} not found`);
+    }
+  }
+}
