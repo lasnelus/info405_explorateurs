e5ce35e46e0c7e1f92a796689d552e742fa8a9a6
synnfall <synnfall@gmail.com>
2026-01-27 08:48:06 +0100

    feat: adding simple getter on guardians without auth for the moment

--- backend/src/guardian/guardian.service.ts
+++ backend/src/guardian/guardian.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { guardianInfoDto } from './dto/guardianDto';
 
 @Injectable()
-export class GuardianService {}
+export class GuardianService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getGuardian(guardianId: string): Promise<guardianInfoDto> {
+    try {
+      return await this.prisma.guardian.findFirstOrThrow({
+        where: {
+          id: guardianId,
+        },
+        omit: {
+          password: true,
+        },
+        include: {
+          families: true,
+        },
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (_) {
+      throw new NotFoundException();
+    }
+  }
+
+  async getAllGuardians(): Promise<guardianInfoDto[]> {
+    return await this.prisma.guardian.findMany({
+      omit: {
+        password: true,
+      },
+      include: {
+        families: true,
+      },
+    });
+  }
+}
