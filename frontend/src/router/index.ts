8b99d67f1b618857f935e08c3c49af4db2c8dbd0
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-02-22 22:24:05 +0100

    feat : debut style.css
    c'est le bordel

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -12,7 +12,17 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+        {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignupView.vue')
+    },
   ],
 })
 
