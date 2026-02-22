dde89c2667f34304c9ec4a14bb75c1bdaf991c6b
synnfall <synnfall@gmail.com>
2026-02-22 16:53:44 +0100

    chore: adding validation pipe for dtos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const cookieName = process.env.REFRESHTOKEN_COOKIE_NAME || 'refresh_token';
 
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
       .setTitle('API')
       .setDescription('Swagger Dev')
       .setVersion('1.0')
+      .addCookieAuth(cookieName, {
+        type: 'apiKey',
+        in: 'cookie',
+        name: cookieName,
+      })
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+        'accessToken',
+      )
       .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('swagger', app, document);
@@ -18,6 +34,13 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
