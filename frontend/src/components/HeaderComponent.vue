ca9a3504da73953ade8188e5b8f7ee621199630f
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-02-23 15:07:47 +0100

    fix : update CSS

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -5,10 +5,18 @@
         <img src="../assets/logoexplorateurV2.1.png" class="h-16 w-auto object-contain" alt="Logo" />
       </router-link>
       <div class="flex gap-4 items-center">
-        <router-link
-          to="/login"
-          class="bg-success px-7 py-2.5 rounded-full font-semibold transition-all duration-300 shadow-md hover:shadow-lg hover:-translate-y-0.5"
-        >
+        <router-link to="/login" class="
+            bg-secondary
+            px-7
+            py-2.5
+            rounded-full
+            font-semibold
+            transition-all
+            duration-300
+            shadow-md
+            hover:shadow-lg
+            hover:-translate-y-0.5
+          ">
           Connexion
         </router-link>
       </div>
@@ -19,5 +27,4 @@
 <script setup lang="ts">
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
