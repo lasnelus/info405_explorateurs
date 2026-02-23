5c46ae604dfca5906a13b70c327bba0434c09465
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-02-23 16:31:42 +0100

    SCRUM-40 # feat : couleurs 👌

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -2,9 +2,9 @@
   <div class="grid justify-center">
 
     <div class="
+      bg-primary-background
       rounded-2xl
       input
-      bg-gris-light
       grid
       justify-center
       p-[40px]
@@ -12,38 +12,16 @@
       w-auto
       my-[50px]
       mx-[44px]
-    "
-    id="signUpBox"
-    style="
-    box-shadow: 4px 4px 15px -8px var(--color-primary);
+    " id="signUpBox" style="
+      box-shadow: 4px 4px 15px -8px var(--color-primary);
     ">
-      <input
-        type="text"
-        id="UserName"
-        name="UserName"
-        placeholder="David"
-      />
+      <input type="text" id="UserName" name="UserName" placeholder="David" class="color-text"/>
 
-      <input
-        type="text"
-        id="EMail"
-        name="EMail"
-        placeholder="david@cmi-info.fr"
-      />
+      <input type="text" id="EMail" name="EMail" placeholder="david@cmi-info.fr" class="color-text"/>
 
-      <input
-        type="password"
-        id="Password"
-        name="Password"
-        placeholder="S0ph!sticat€dP@ssw0rd"
-      />
+      <input type="password" id="Password" name="Password" placeholder="S0ph!sticat€dP@ssw0rd" class="color-text"/>
 
-      <input
-        type="password"
-        id="PasswordConfirm"
-        name="PasswordConfirm"
-        placeholder="S0ph!sticat€dP@ssw0rd"
-      />
+      <input type="password" id="PasswordConfirm" name="PasswordConfirm" placeholder="S0ph!sticat€dP@ssw0rd" class="color-text"/>
     </div>
 
     <div id="switchToSignInBox">
@@ -57,18 +35,24 @@
 </script>
 
 <style scoped>
-
 input {
   width: calc(inherit - 60px);
-  border-style : var(--tw-border-style);
-  border-width : 0px;
-  border-radius:var(--radius-lg);
+  border-style: var(--tw-border-style);
+  border-width: 0px;
+  border-radius: var(--radius-lg);
   padding-block: calc(var(--spacing) * 3);
   padding-inline: calc(var(--spacing) * 5);
   --tw-outline-style: none;
   outline-style: none;
-  background-color: var(--color-white);
-  margin: 10px 0
+  background-color: rgba( from var(--color-primary-background) calc(r + 25)  calc(g + 25) calc(b + 25) / .75);
+  margin: 10px 0;
+
+  transition: var(--default-transition-duration);
+}
+
+input:hover, input:active {
+  background-color: rgba( from var(--color-primary) r g b / .15);
+  transition: var(--default-transition-duration);
 }
 
 </style>
