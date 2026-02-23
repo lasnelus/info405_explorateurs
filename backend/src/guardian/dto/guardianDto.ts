d43f7a0362202d33fbd3d5c9a860e99bca4e775c
synnfall <synnfall@gmail.com>
2026-02-23 22:09:34 +0100

    SCRUM-50 #feat adding register route for guardians

--- backend/src/guardian/dto/guardianDto.ts
+++ backend/src/guardian/dto/guardianDto.ts
@@ -1,4 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class guardianInfoDto {
   @ApiProperty({
@@ -41,4 +49,51 @@ export class guardianInfoDto {
     updatedAt: Date;
     name: string;
   }[];
+}
+
+export class registerGuardianCredentials {
+  @ApiProperty({
+    example: 'david@exemple.fr',
+  })
+  @IsEmail({}, { message: 'Email invalide' })
+  @MaxLength(64, {
+    message: "L'Email doit contenir au plus 64 caractères",
+  })
+  email: string;
+
+  @ApiProperty({
+    example: 'sd@pRKT#AyBY8FFD',
+  })
+  @IsNotEmpty({ message: 'Le mot de passe est requis' })
+  @MinLength(8, {
+    message: 'Le mot de passe doit contenir au moins 8 caractères',
+  })
+  @MaxLength(32, {
+    message: 'Le mot de passe doit contenir au plus 32 caractères',
+  })
+  password: string;
+
+  @ApiProperty({
+    example: 'Gerald',
+  })
+  @IsString()
+  @MaxLength(80, {
+    message: 'Le prénom doit contenir au plus 80 caractères',
+  })
+  @Matches(/^[A-Za-zÀ-ÖØ-öø-ÿ'-]+$/, {
+    message: 'Le prénom contient des caractères non autorisés !',
+  })
+  firstName: string;
+
+  @ApiProperty({
+    example: 'Kant',
+  })
+  @IsString()
+  @MaxLength(100, {
+    message: 'Le nom de famille doit contenir au plus 100 caractères',
+  })
+  @Matches(/^[A-Za-zÀ-ÖØ-öø-ÿ'-]+$/, {
+    message: 'Le nom contient des caractères non autorisés !',
+  })
+  lastName: string;
 }
--- backend/src/guardian/guardian.service.ts
+++ backend/src/guardian/guardian.service.ts
@@ -1,10 +1,25 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserPayload } from './../auth/dto/payload';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { guardianInfoDto } from './dto/guardianDto';
+import {
+  guardianInfoDto,
+  registerGuardianCredentials,
+} from './dto/guardianDto';
+import { Response } from 'express';
+import { hash } from 'bcrypt';
+import { StringValue } from 'ms';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class GuardianService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async getGuardian(guardianId: string): Promise<guardianInfoDto> {
     try {
@@ -16,7 +31,14 @@ export class GuardianService {
           password: true,
         },
         include: {
-          families: true,
+          families: {
+            select: {
+              id: true,
+              createdAt: true,
+              updatedAt: true,
+              name: true,
+            },
+          },
         },
       });
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -34,5 +56,71 @@ export class GuardianService {
         families: true,
       },
     });
+  }
+
+  async register(
+    registerBody: registerGuardianCredentials,
+    res: Response,
+  ): Promise<void> {
+    const hashedPassword = await this.hashPassword(registerBody.password);
+
+    if (await this.findByEmail(registerBody.email)) {
+      throw new ConflictException();
+    }
+
+    const user = await this.prisma.guardian.create({
+      data: {
+        email: registerBody.email,
+        firstName: registerBody.firstName,
+        lastName: registerBody.lastName,
+        password: hashedPassword,
+      },
+    });
+
+    const payLoad: UserPayload = { role: user.role, userId: user.id };
+    const refreshToken = this.getRefreshToken(payLoad);
+
+    this.putRefreshTokenCookies(res, refreshToken);
+  }
+
+  private putRefreshTokenCookies(res: Response, refreshToken: string): void {
+    res.cookie(
+      process.env.REFRESHTOKEN_COOKIE_NAME || 'refresh_token',
+      refreshToken,
+      {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge:
+          1000 *
+          (parseInt(process.env.REFRESH_TOKEN_EXPIRES_IN || '604800', 10) ||
+            604800), // 1 jour en millisecondes
+        path: '/',
+      },
+    );
+  }
+
+  private async findByEmail(email: string) {
+    const [manager, guardian] = await Promise.all([
+      this.prisma.manager.findUnique({ where: { email } }),
+      this.prisma.guardian.findUnique({ where: { email } }),
+    ]);
+
+    return manager ?? guardian;
+  }
+
+  private getRefreshToken(payLoad: UserPayload): string {
+    let expiration: StringValue = '7d' as StringValue;
+    if (process.env.REFRESH_TOKEN_EXPIRES_IN) {
+      expiration = (process.env.REFRESH_TOKEN_EXPIRES_IN + 's') as StringValue;
+    }
+    return this.jwtService.sign(payLoad, {
+      secret: process.env.REFRESHTOKEN_SECRET,
+      expiresIn: expiration,
+    });
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return await hash(password, 10);
   }
 }
