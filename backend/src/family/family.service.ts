ea3c57e1562a4ce234ea6a7978d7f564b3c41bbf
synnfall <synnfall@gmail.com>
2026-03-12 13:38:34 +0100

    feat: adding post and get family

--- backend/src/family/family.service.ts
+++ backend/src/family/family.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { FamilyCreationDto, FamilyDto } from './dto/familyDto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class FamilyService {}
+export class FamilyService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createFamily(familyBody: FamilyCreationDto) {
+    const data: Prisma.FamilyCreateInput = {
+      name: familyBody.name,
+    };
+
+    if (familyBody.guardianId) {
+      const guardian = await this.prisma.guardian.findUnique({
+        where: { id: familyBody.guardianId },
+      });
+      if (!guardian) throw new NotFoundException('Guardian not found');
+      data.guardians = {
+        connect: { id: familyBody.guardianId },
+      };
+    }
+
+    await this.prisma.family.create({ data });
+  }
+
+  async getFamilyById(familyId: string): Promise<FamilyDto> {
+    const family = await this.prisma.family.findUnique({
+      where: {
+        id: familyId,
+      },
+      include: {
+        childs: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+          },
+        },
+        guardians: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+          },
+        },
+      },
+    });
+    if (!family) throw new NotFoundException('family not found');
+    return family;
+  }
+
+  async isGuardianInFamily(
+    guardianId: string,
+    familyId: string,
+  ): Promise<boolean> {
+    const family = await this.prisma.family.findUnique({
+      where: {
+        id: familyId,
+        guardians: {
+          some: {
+            id: guardianId,
+          },
+        },
+      },
+    });
+    return !!family;
+  }
+}
