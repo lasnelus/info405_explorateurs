ef16aad17a2c76175699f95e54e1db18cbd31bb9
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-03-13 20:38:38 +0100

    SCRUM-98 #feat : Boutons de control du theme + storage

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
       <button
         v-if="isLoggedIn"
         @click="toggleMenu"
-        class="flex flex-col gap-1 w-8"
+        class="flex flex-col gap-1 w-8 hover:cursor-pointer"
       >
         <span class="h-1 bg-secondary rounded"></span>
         <span class="h-1 bg-secondary rounded"></span>
@@ -51,23 +51,109 @@
     duration-300"
     :class="menuOpen ? 'translate-x-0' : 'translate-x-full'"
   >
-    <div class="p-6 flex flex-col gap-6 h-full">
+    <div class="p-6 flex flex-col justify-between gap-6 h-full">
       <nav class="flex flex-col gap-4 font-semibold text-lg">
-        <router-link to="/family-dashboard" @click="closeMenu">Ma famille</router-link>
-        <router-link to="/" @click="closeMenu">lorem ipsum</router-link>
-        <router-link to="/" @click="closeMenu">lorem ipsum</router-link>
-        <router-link to="/" @click="closeMenu">Inscrire un enfant</router-link>
+        <router-link to="/family-dashboard" @click="closeMenu" class="
+          -my-1
+          py-1
+          text-center
+          rounded-3xl
+          duration-300
+          text-text
+          bg-primary/25
+          hover:bg-primary/50
+        ">Ma famille</router-link>
+        <router-link to="/" @click="closeMenu" class="
+          -my-1
+          py-1
+          text-center
+          rounded-3xl
+          duration-300
+          text-text
+          bg-primary/25
+          hover:bg-primary/50
+        ">lorem ipsum</router-link>
+        <router-link to="/" @click="closeMenu" class="
+          -my-1
+          py-1
+          text-center
+          rounded-3xl
+          duration-300
+          text-text
+          bg-primary/25
+          hover:bg-primary/50
+        ">lorem ipsum</router-link>
+        <router-link to="/" @click="closeMenu" class="
+          -my-1
+          py-1
+          text-center
+          rounded-3xl
+          duration-300
+          text-text
+          bg-primary/25
+          hover:bg-primary/50
+        ">Inscrire un enfant</router-link>
 
         <div class="mt-auto">
           <button
             @click="logout"
-            class="block w-full text-center bg-red-500 text-white py-3 rounded-full font-semibold"
+            class="block w-full text-center bg-red-500 text-white py-3 rounded-full font-semibold cursor-pointer
+             duration-300 shadow-md shadow-primary-background transition-all
+            hover:scale-105 hover:shadow-lg hover:shadow-red-500/50
+            "
           >
             Déconnexion
           </button>
         </div>
       </nav>
 
+      <!-- THEME & ACCENT -->
+      <div class="
+        mb-5
+        justify-self-end
+        flex
+        flex-col
+
+      ">
+      <!-- Theme -->
+        <div>
+          <button
+          @click="changeTheme('dark')"
+          >
+            Sombre
+          </button>
+          <button
+          @click="changeTheme('light')"
+          >
+            Clair
+          </button>
+        </div>
+
+        <!-- Accent -->
+        <div>
+          <button
+          @click="changeAccent('bleu')"
+          >
+            Bleu
+          </button>
+          <button
+          @click="changeAccent('vert')"
+          >
+            Vert
+          </button>
+          <button
+          @click="changeAccent('jaune')"
+          >
+            Jaune
+          </button>
+          <button
+          @click="changeAccent('rouge')"
+          >
+            Rouge
+          </button>
+        </div>
+
+      </div>
     </div>
   </aside>
 </template>
@@ -76,6 +162,7 @@
 import { ref, computed } from "vue"
 import { useRouter } from "vue-router"
 import { useAuthStore } from "@/stores/auth"
+import { useColorStore } from "@/stores/colorStore"
 
 const router = useRouter()
 const auth = useAuthStore()
@@ -99,4 +186,48 @@ function logout() {
   closeMenu()
   router.push("/login")
 }
+
+// ----
+// Gestion des thèmes
+// ----
+
+function changeTheme(theme : string) {
+
+  // Récup des thèmes stoqués
+  const color = useColorStore()
+
+  const oldTheme  = color.colorTheme
+
+  // Modification du store après récup
+  color.setColorTheme(theme)
+
+  // Vérification additionelle si le theme ne change pas, on ne fait rien.
+  if (theme != oldTheme) {
+    // Si le theme est set dans le colorStore (elle existe donc dans l'app), on se permet d'enlever la couleur du body
+    if (oldTheme != null) {
+      document.body.classList.remove(oldTheme)
+    }
+    document.body.classList.add(theme)
+  }
+}
+
+function changeAccent(accent : string) {
+
+  // Récup des thèmes stoqués
+  const color = useColorStore()
+
+  const oldAccent = color.colorAccent
+
+  // Modification du store après récup
+  color.setColorAccent(accent)
+
+  // Vérification additionelle si l'accent ne change pas, on ne fait rien.
+  if (accent != oldAccent) {
+    if (oldAccent != null) {
+      document.body.classList.remove(oldAccent)
+    }
+    document.body.classList.add(accent)
+  }
+}
+
 </script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useColorStore } from '@/stores/colorStore'
 
 import App from './App.vue'
 import router from './router'
@@ -7,7 +8,19 @@ import './style.css'
 
 const app = createApp(App)
 
+
 app.use(createPinia())
 app.use(router)
+
+const color = useColorStore()
+const accent = color.colorAccent
+
+const theme  = color.colorTheme
+
+document.body.classList.add(accent)
+document.body.classList.add(theme)
+
+console.log("Theme: " + theme)
+console.log("Couleur d'accent : " + accent)
 
 app.mount('#app')
