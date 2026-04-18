a5d1a64a28496d31bd6d694b89efe86b1610885b
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-04-18 18:21:17 +0200

    feat : Reflexions features du pannel admin

--- frontend/src/components/admin/StaffManagement.vue
+++ frontend/src/components/admin/StaffManagement.vue
@@ -4,4 +4,17 @@
     STAFF MANAGEMENT
 
   </h1>
+
+  <p>
+
+    Ajouter un animateur
+
+  </p>
+
+  <p>
+
+    Tableau des animateurs : ID_ANIM | nom | prenom | groupe | est_present_aujourd_hui | modif/supr
+
+  </p>
+
 </template>
--- frontend/src/components/admin/TomorowInfo.vue
+++ frontend/src/components/admin/TomorowInfo.vue
@@ -1,5 +1,18 @@
 <template>
   <h1>
-  TOMORROW
+
+    TOMORROW
+
   </h1>
+  <p>
+
+    Enfants attendus <br>
+    Tableau : ID_ENFANT | nom | prenom | regime_special | [si regime_special]->[type_regime] | [si owner]->[accès fiche enfant]
+
+  </p>
+  <p>
+
+    tab recap repas : ID_REPAS | type | nb_unit | est_regime_special | [si regime_special]->[type_regime]
+
+  </p>
 </template>
