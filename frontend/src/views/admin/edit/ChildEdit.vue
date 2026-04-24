a539dd1795eb49a4037f343767f361ae623a1183
lasnelus <malabretienne@gmail.com>
2026-04-24 11:15:21 +0200

    feat: start for edit

--- frontend/src/views/admin/edit/ChildEdit.vue
+++ frontend/src/views/admin/edit/ChildEdit.vue
@@ -1,3 +1,35 @@
 <template>
-    <p>pizza</p>
+    <div class="bg-primary/5 rounded-lg shadow-lg shadow-primary/15 p-8">
+        <h1 class="text-3xl font-bold mb-4 text-primary">
+            pizzza
+            <!-- {{ child.firstName }} {{ child.lastName }} -->
+          </h1>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { getChild } from '@/services/familyServices';
+import { role } from '@/services/authServices'
+
+const route = useRoute()
+const router = useRouter()
+
+
+const childId = route.query.childID
+
+const child = ref(null)
+
+const roleUser = await role()
+
+onMounted(async () =>{
+    if (roleUser.data.role != 'OWNER' && roleUser.data.role != 'INSTRUCTOR') {
+        router.push('/login')
+    }else{
+        const res = await getChild(childId)
+        child.value = res.data
+        console.log(child)
+    }
+})
+</script>
