7d2867b58ae9e3ea7810bb74a44d826fbec43f27
Th3_Warior <ewenbellot73@gmail.com>
2026-05-10 23:22:36 +0200

    feat : familles a la creation d'enfants

--- frontend/src/views/admin/create/ChildCreate.vue
+++ frontend/src/views/admin/create/ChildCreate.vue
@@ -59,12 +59,37 @@
 
         <div class="bg-primary/5 rounded-lg shadow-lg shadow-primary/15 p-8 mb-8">
           <h2 class="text-2xl font-bold mb-6 text-primary">Familles</h2>
-          <input
-            v-model="newChild.familyIds"
-            type="text"
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <ul v-if="newChild.familyIds.length > 0" class="mb-3 space-y-2">
+              <li
+                v-for="id in newChild.familyIds"
+                :key="id"
+                class="flex items-center justify-between bg-primary/20 border border-primary/30 px-3 py-2 rounded-lg text-text"
+              >
+                <span>{{ getFamilyName(id) }}</span>
+
+                <button
+                  type="button"
+                  @click="removeFamilyFromChild(id)"
+                  class="text-text bg-error rounded-full cursor-pointer font-bold hover:bg-error/75 px-2"
+                  title="Retirer cette famille"
+                >
+                  Retirer
+                </button>
+              </li>
+            </ul>
+          </div>
+          <select
+            v-model="familyToAdd"
+            @change="addFamilyToChild"
             class="w-full rounded-lg border border-primary/20 bg-primary-background/50 px-4 py-2 text-text focus:outline-none focus:ring-2 focus:ring-primary/30"
-            placeholder="Dupont"
-          />
+          >
+            <option disabled value="">-- Ajouter une famille --</option>
+
+            <option v-for="famille in availableFamilies" :key="famille.id" :value="famille.id">
+              {{ famille.name }}
+            </option>
+          </select>
         </div>
 
         <div class="bg-success/25 rounded-lg shadow-lg shadow-success/35 p-8">
@@ -95,10 +120,11 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, reactive, ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { role } from '@/services/authServices'
 import { addNewChild } from '@/services/childServices'
+import { getFamilies } from '@/services/familyServices'
 
 const router = useRouter()
 
@@ -111,8 +137,37 @@ const newChild = reactive({
   lastName: '',
   birthDate: '',
   foodConstraint: 'NONE',
-  familyIds: [],
+  familyIds: [] as string[],
 })
+
+// ---------------------------
+// Gestion d'ajout de familles
+
+const familyToAdd = ref('')
+
+const availableFamilies = computed(() => {
+  return allFamillies.value.filter((famille) => !newChild.familyIds.includes(famille.id))
+})
+
+const addFamilyToChild = () => {
+  if (familyToAdd.value && !newChild.familyIds.includes(familyToAdd.value)) {
+    newChild.familyIds.push(familyToAdd.value)
+    familyToAdd.value = ''
+  }
+}
+
+const removeFamilyFromChild = (idToRemove: string) => {
+  newChild.familyIds = newChild.familyIds.filter((id) => id != idToRemove)
+}
+
+const getFamilyName = (id: string) => {
+  const family = allFamillies.value.find((f) => f.id === id)
+  return family ? family.name : 'Inconnue'
+}
+
+// --- --- --- --- --- --- ---
+
+const allFamillies = ref([])
 
 async function createNewChild() {
   errorMessage.value = ''
@@ -153,6 +208,15 @@ const foodConstraintsOptions = [
 onMounted(async () => {
   if (roleUser.data.role != 'OWNER') {
     router.push('/login')
+  }
+
+  // --- GET FAMILLES ---
+  try {
+    const familliesResponse = await getFamilies()
+    allFamillies.value = familliesResponse.data
+    console.log(allFamillies.value)
+  } catch (error) {
+    console.error('Erreur lors de la récupération des enfants:', error)
   }
 })
 </script>
