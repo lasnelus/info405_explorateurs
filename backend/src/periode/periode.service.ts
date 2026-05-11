177474e8edb3d5f91f02ce327e89d1c36b3f41bd
synnfall <synnfall@gmail.com>
2026-05-11 13:06:03 +0200

    feat: ajout de la bonne file d'attente

--- backend/src/periode/periode.service.ts
+++ backend/src/periode/periode.service.ts
@@ -28,8 +28,10 @@ export class PeriodeService {
 
   @Cron(CronExpression.EVERY_10_MINUTES)
   async updateQueue() {
+    console.log('UPDATE QUEUE');
     const nowPlusOneDay = new Date(Date.now());
     nowPlusOneDay.setDate(nowPlusOneDay.getDate() - 1);
+
     await this.prisma.queue.updateMany({
       where: {
         acceptedAt: {
@@ -61,33 +63,125 @@ export class PeriodeService {
   }
 
   async updateDayInPeriode(periodeId: string, day: Date) {
+    if (periodeId != 'cmp0e5jyc00008njmh3960b9i') return;
     const capacity = (await this.getPeriodesById(periodeId))!.capacity;
+    console.log('day', day);
     const slots = await this.countSlots(periodeId, day);
     const queueAccepted = await this.countQueuesAccepted(periodeId, day);
     const toUpdate: number = Math.max(0, capacity - slots - queueAccepted);
-    const queueToAccept = await this.prisma.queue.findMany({
+    console.log(`id: ${periodeId}`);
+    console.log('coucou');
+    console.log(
+      `capacity : ${capacity}, slots: ${slots}, accepted: ${queueAccepted}  toUpdate: ${toUpdate}`,
+    );
+    // d'abord on récup toute les queues avec leurs familles pour voir si d'autre membre de leur familles sont inscrits se jour la
+    const pendingQueues = await this.prisma.queue.findMany({
       where: {
         periodeId,
         day,
         state: 'PENDING',
       },
-      orderBy: {
-        createdAt: 'asc',
+      include: {
+        child: {
+          include: {
+            families: {
+              include: {
+                childs: {
+                  include: {
+                    slots: {
+                      where: { periodeId, day },
+                      select: { id: true },
+                    },
+                    queues: {
+                      where: { periodeId, day, state: 'ACCEPTED' },
+                      select: { id: true },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
       },
-      take: toUpdate,
     });
 
+    // Calculer le nombre de membre de la même famille déjà inscrits pour chaque demande
+    const queuesWithPriority = pendingQueues.map((queue) => {
+      let memberAlreadyInSlot = 0;
+      const familyMemberFind = new Set();
+
+      queue.child.families.forEach((family) => {
+        family.childs.forEach((sibling) => {
+          // On s'assure de ne pas compter l'enfant lui même ou  de compter un enfant deux fois
+          if (
+            sibling.id !== queue.child.id &&
+            !familyMemberFind.has(sibling.id)
+          ) {
+            familyMemberFind.add(sibling.id);
+
+            // Un membre de la famille est déjà s'il a un Slot ou si sa Queue est ACCEPTED
+            const isChildOk =
+              sibling.slots.length > 0 || sibling.queues.length > 0;
+            if (isChildOk) {
+              memberAlreadyInSlot++;
+            }
+          }
+        });
+      });
+
+      return {
+        ...queue,
+        memberAlreadyInSlot,
+      };
+    });
+
+    // trie les demandes par ordre de prio
+    let remainingItems = queuesWithPriority.length;
+    let isSorted: boolean;
+
+    do {
+      isSorted = true;
+
+      for (let i = 0; i < remainingItems - 1; i++) {
+        const current = queuesWithPriority[i];
+        const next = queuesWithPriority[i + 1];
+        let requiresSwap = false;
+
+        // d'abord par les gens de la même famille déjà inscrits
+        if (current.memberAlreadyInSlot < next.memberAlreadyInSlot) {
+          requiresSwap = true;
+        }
+        // puis par date d'inscription
+        else if (current.memberAlreadyInSlot === next.memberAlreadyInSlot) {
+          if (current.createdAt.getTime() > next.createdAt.getTime()) {
+            requiresSwap = true;
+          }
+        }
+
+        // echange des elements sans passer par une variable temporaire
+        if (requiresSwap) {
+          [queuesWithPriority[i], queuesWithPriority[i + 1]] = [
+            queuesWithPriority[i + 1],
+            queuesWithPriority[i],
+          ];
+          isSorted = false;
+        }
+      }
+      remainingItems--;
+    } while (!isSorted);
+
+    // on fini la liste avec que les queue a accetper
+    const queueToAccept = queuesWithPriority.slice(0, toUpdate);
+
+    // et maintenant on accepte des gens
     for (const queue of queueToAccept) {
       await this.prisma.queue.update({
-        where: {
-          id: queue.id,
-        },
+        where: { id: queue.id },
         data: {
           state: 'ACCEPTED',
           acceptedAt: new Date(Date.now()),
         },
       });
-
       await this.mailService.sendAcceptedEmail(queue.childId);
     }
   }
